755acf8587f1d446a36346cc326259e24531de3c
mrfalone <mrfalone00@gmail.com>
2025-01-04 12:02:07 +0100

    bir fotograf eklendi

--- project.py
+++ project.py
@@ -1,4 +1,15 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk  # Pillow kütüphanesi
 
-ekran = Tk()
-ekran.title("Sports coaching")
+ekran1 = tk()
+ekran1.title("Sport coaching app")
+ekran1.geometry("400c800")
+ekran1.resizable(False, False)
+
+
+img = PhotoImage(file = "images\photo1.jpg")
+img_label = Label(ekran1, image=img)
+img_label.place(x=-45, y=-150)
+
+ekran1.mainloop()
