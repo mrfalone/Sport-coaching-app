9287cd2fbbe7363cbaae75d0697696cc0ed03fd8
mrfalone <mrfalone00@gmail.com>
2025-01-06 17:56:12 +0100

    toplevel kodlarini yazmaya baslandi

--- project.py
+++ project.py
@@ -1,61 +1,28 @@
-import tkinter as tk
 from tkinter import *
-from PIL import Image, ImageTk  # Pillow kütüphanesi
 
-ekran1 = Tk()
-ekran1.title("Sport coaching app")
-ekran1.geometry("400x800")
-ekran1.resizable(False, False)
 
-ekran1 = Tk()
-ekran1.title("Sport coaching app")
-ekran1.geometry("400x800")
-ekran1.resizable(False, False)
-
-ekran2 = Tk()
-ekran2.title("Sport coaching app")
-ekran2.geometry("400x800")
-ekran2.resizable(False, False)
-
-ekran3 = Tk()
-ekran3.title("Sport coaching app")
-ekran3.geometry("400x800")
-ekran3.resizable(False, False)
-
-ekran4 = Tk()
-ekran4.title("Sport coaching app")
-ekran4.geometry("400x800")
-ekran4.resizable(False, False)
-
-ekran5 = Tk()
-ekran5.title("Sport coaching app")
-ekran5.geometry("400x800")
-ekran5.resizable(False, False)
-
-img = PhotoImage("photo1.jpg")
-img_label = Label(ekran1, image=img)
-img_label.place(x=-100, y=300)
-
-img2 = PhotoImage("photo2.jpg")
-img_label = Label(ekran1, image=img2)
-img_label.place(x=-100, y=300)
-
-img3 = PhotoImage("photo3.jpg")
-img_label = Label(ekran1, image=img3)
-img_label.place(x=-100, y=300)
-
-img4 = PhotoImage("photo4.jpg")
-img_label = Label(ekran1, image=img4)
-img_label.place(x=-100, y=300)
-
-img5 = PhotoImage("photo5.jpg")
-img_label = Label(ekran1, image=img5)
-img_label.place(x=-100, y=300)
+ekran=Tk()
+ekran.title("Sport Center Project")
+ekran.geometry("300x500+500+200")
 
 
 
+def register():
+    registerPage=Toplevel(ekran)
+
+def Login():
+    loginPage=Toplevel(ekran)
+    loginPage.title("Login Page")
+    loginPage.geometry("400x600+500+200")
+    AdLabel=Label(loginPage,text="Adınızı Girin:")
+    AdLabel.place(x=100,y=100)
+  
 
 
 
+loginButton=Button(ekran,text="Login Page",command=Login)
+loginButton.place(x=150,y=100)
 
-ekran1.mainloop()
+
+
+ekran.mainloop()
