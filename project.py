f180713a0a18aa8247365c564501c5523d21da17
mrfalone <mrfalone00@gmail.com>
2025-01-26 10:25:27 +0100

    database eklendi

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from tkinter import *
-
+from tkinter import messagebox
+import sqlite3
 
 ekran=Tk()
 ekran.title("Sport Center Project")
@@ -7,6 +8,190 @@ ekran.geometry("370x600+500+200")
 ekran.config(bg = "#8D7160")
 ekran_icon = PhotoImage(file=r"images\icon.png")
 ekran.iconphoto(False, ekran_icon)
+
+
+listeKutusu=""
+listelePenceresi=""
+# ---------------------------------------------------------------------------- #
+#                          VERITABANININ OLUŞTURULMASI                         #
+# ---------------------------------------------------------------------------- #
+# Veritabanı dosyamızın oluşturulması (Bağlantı oluşturulması)
+
+kisiselbilgi=sqlite3.connect("kisiselbilgi.db")
+curr=kisiselbilgi.cursor()
+# ---------------------------------------------------------------------------- #
+# Kitap Kaydı Tablosunun Oluşturulması
+#IF NOT EXISTS  ekleyerek defalarca aynı tablos oluşuturulmasını istek yapmıyoruz.Bu da hata almamızı engelliyor
+curr.execute(''' CREATE TABLE IF NOT EXISTS kitapKaydi (
+  ID INT PRIMARY KEY,
+  name VARCHAR(50),
+  username VARCHAR(15),
+  age INTEGER,
+  password INTEGER
+  )   ''')
+
+kisiselbilgi.commit()
+kisiselbilgi.close()
+
+# ---------------------------------------------------------------------------- #
+# YENİ ELEMAN KAYIT İŞLEME
+def Kaydet():
+    userID=ID.get()
+    isim=name.get()
+    nickname=username.get()
+    sifre=password.get()
+    yas=age.get()
+
+    # Veritabanı bağlantısı
+    kisiselbilgi=sqlite3.connect("kisiselbilgi.db")
+    curr=kisiselbilgi.cursor()
+    curr.execute(''' INSERT INTO kitapKaydi (ID,name,username,password,age) VALUES 
+    (?,?,?,?)''',(userID,isim,nickname,sifre,yas))
+    kisiselbilgi.commit()
+    kisiselbilgi.close()
+
+# ---------------------------------------------------------------------------- #
+def duzenle():
+      secilen=listeKutusu.curselection()
+      secilmis=secilen[0]
+    
+      bilgi=listeKutusu.get(secilen)
+      print(bilgi[0])
+      
+      guncellePenceresi=Toplevel(listelePenceresi)
+      guncellePenceresi.geometry("250x250")
+      guncellePenceresi.title("GÜNCELLE")
+      
+      Label(guncellePenceresi, text="UserID:").place(x=20, y=10)
+      uyeAdi = Entry(guncellePenceresi)
+      uyeAdi.place(x=80, y=10)
+
+      Label(guncellePenceresi, text="Isim:").place(x=20, y=40)
+      ISBN = Entry(guncellePenceresi)
+      ISBN.place(x=80, y=40)
+
+      Label(guncellePenceresi, text="NickName:").place(x=20, y=80)
+      AlınısTarihi = Entry(guncellePenceresi)
+      AlınısTarihi.place(x=80, y=80)
+      
+      
+      def guncelleme():
+              userID=ID.get()
+              isim=name.get()
+              nickname=username.get()
+              sifre=password.get()
+              yas=age.get()
+              
+              #veritabanı bağlantısını oluşutrduk
+              kisiselbilgi=sqlite3.connect("kisiselbilgi.db")
+              curr=kisiselbilgi.cursor()
+              curr.execute(''' INSERT INTO kitapKaydi (ID,name,username,password,age) VALUES 
+              (?,?,?,?)''',(userID,isim,nickname,sifre,yas))
+              kisiselbilgi.commit()
+              kisiselbilgi.close() 
+                
+              Listele()   
+
+      guncelleButon=Button(guncellePenceresi,text="GUNCELLE",command=guncelleme)
+      guncelleButon.place(x=120,y=120)
+      
+     
+      
+def sil():
+      
+      cevap=messagebox.askokcancel("Onay Kutusu","Silmek istediğinize emin misiniz?:")
+      
+      if cevap:          
+          secilen=listeKutusu.curselection()
+          secilmis=secilen[0]        
+          bilgi=listeKutusu.get(secilen)
+          print(bilgi[0])
+          #veritabanı bağlantısını oluşutrduk
+          kisiselbilgi=sqlite3.connect("kisiselbilgi.db")
+          curr=kisiselbilgi.cursor()
+          curr.execute(''' DELETE FROM kisiselbilgi WHERE userID=?''',(bilgi[0],)  )
+          kisiselbilgi.commit()
+          kisiselbilgi.close() 
+          Listele()
+
+
+
+
+
+def Listele():
+      global listeKutusu
+      global listelePenceresi
+      listelePenceresi=Toplevel(ekran)
+      listelePenceresi.geometry("350x500+500+300")
+      listelePenceresi.title("Listeleme Ekranı")
+      
+      duzenleButton=Button(listelePenceresi,text="DÜZENLE",command=duzenle)
+      duzenleButton.place(x=280,y=50)
+      
+      silButton=Button(listelePenceresi,text="KAYIT SİL",command=sil)
+      silButton.place(x=280,y=90)
+      
+      
+      # Veritabanı bağlantısı
+      kisiselbilgi=sqlite3.connect("kisiselbilgi.db")
+      curr=kisiselbilgi.cursor()
+      # Yapılacak işlemler bu arada yapılıyor. Sadece bu kısım değişiyor
+      curr.execute(''' SELECT * FROM  kisiselbilgi''')
+      # fetch işlemi bir şeyi al getir anlamında. Burdada verileri çekmek için kullanılır.
+      bilgiler=curr.fetchall()
+  
+      kisiselbilgi.close()
+      listeKutusu=Listbox(listelePenceresi,height=20,width=40)
+      listeKutusu.place(x=20,y=10)
+    
+      
+      for bilgi in bilgiler:
+            
+            listeKutusu.insert(END,bilgi)
+      
+
+
+
+
+
+
+
+# # Resim Ekleme
+# resim = Image.open("images\kitapKurdu.png")  # Resmi aç
+# resim = resim.resize((200, 200), Image.ANTIALIAS)  # Resmi boyutlandır
+# resim = ImageTk.PhotoImage(resim)  # Resmi Tkinter PhotoImage nesnesine dönüştür
+# etiket = Label(ekran, image=resim)  # Etiket oluştur
+# etiket.place(x=500, y=30)  # Sağ üst köşeye yerleştir
+
+# ---------------------------------------------------------------------------- #
+# Girişler ve Etiketler
+Label(ekran, text="Üye ID:").place(x=100, y=20)
+userID = Entry(ekran)
+userID.place(x=200, y=20)
+
+Label(ekran, text="Üye Adı:").place(x=100, y=50)
+isim = Entry(ekran)
+isim.place(x=200, y=50)
+
+Label(ekran, text="Üye nickname:").place(x=100, y=50)
+nickname = Entry(ekran)
+nickname.place(x=200, y=80)
+
+Label(ekran, text="Yas:").place(x=100, y=80)
+yas = Entry(ekran)
+yas.place(x=200, y=120)
+
+Label(ekran, text="Sifre:").place(x=100, y=110)
+sifre = Entry(ekran)
+sifre.place(x=200, y=150)
+
+# ---------------------------------------------------------------------------- #
+# Kaydet Butonu
+kaydetButon = Button(ekran, text="KAYDET", command=Kaydet)
+kaydetButon.place(x=150, y=140) 
+
+listeleButton=Button(ekran,text="LİSTELE",command=Listele)
+listeleButton.place(x=210,y=140)
 
 #img = PhotoImage(file = "D:\python_proje\Sport-coaching-app\images\menu_photo.jpg")
 #img_label = Label(ekran, image=img)
@@ -20,18 +205,20 @@ def register():
     registerPage_icon = PhotoImage(file=r"images\icon.png")
     registerPage.iconphoto(False, registerPage_icon)
 
-    AdLabel=Label(registerPage,text="PERSONAL SPORT COACHING APP")
-    Label.config(font = ("Inter 10"))
+    AdLabel=Label(registerPage,text="PERSONAL SPORT COACHING APP", font = ("Inter 10"))
     AdLabel.place(x=56,y=3)
     
     AdLabel=Label(registerPage,text="Name:", bg = "#8D7160")
     AdLabel.place(x=45,y=80)
     
-    AdLabel=Label(registerPage,text="Mail:", bg = "#8D7160")
+    AdLabel=Label(registerPage,text="Username:", bg = "#8D7160")
     AdLabel.place(x=45,y=100)
     
-    AdLabel=Label(registerPage,text="Password:", bg = "#8D7160")
+    AdLabel=Label(registerPage,text="age:", bg = "#8D7160")
     AdLabel.place(x=45,y=120)
+
+    AdLabel=Label(registerPage,text="Password:", bg = "#8D7160")
+    AdLabel.place(x=45,y=150)
 
 def Login():
     loginPage=Toplevel(ekran)
@@ -41,8 +228,7 @@ def Login():
     loginPage_icon = PhotoImage(file=r"images\icon.png")
     loginPage.iconphoto(False, loginPage_icon)
 
-    AdLabel=Label(loginPage,text="PERSONAL SPORT COACHING APP")
-    Label.config(font = ("Inter 10"))
+    AdLabel=Label(loginPage,text="PERSONAL SPORT COACHING APP",font = ("Inter 10"))
     AdLabel.place(x=56,y=3)
 
     AdLabel=Label(loginPage,text="Name:",bg = "#8D7160")
@@ -62,8 +248,7 @@ def personal():
     personalPage_icon = PhotoImage(file=r"images\icon.png")
     personalPage.iconphoto(False, personalPage_icon)
 
-    AdLabel=Label(personalPage,text="PERSONAL SPORT COACHING APP")
-    Label.config(font = ("Inter 10"))
+    AdLabel=Label(personalPage,text="PERSONAL SPORT COACHING APP", fg=("black"),font = ("Inter 10"))
     AdLabel.place(x=56,y=3)
 
 
@@ -73,8 +258,7 @@ def personal():
 
 
 
-AdLabel=Label(ekran,text="PERSONAL SPORT COACHING APP")
-AdLabel.config(font = ("Inter 10"))
+AdLabel=Label(ekran,text=("PERSONAL SPORT COACHING APP"),font = ("Inter 10"))
 AdLabel.place(x=56,y=3)
 
 loginButton=Button(ekran,text="Login Page",command=Login)
@@ -85,5 +269,14 @@ signupButton.place(x=140,y=36)
 
 personalButton=Button(ekran,text="Personal Information",command=personal)
 personalButton.place(x=233,y=36)
+
+
+
+
+
+
+
+
+ekran.mainloop()
 
 ekran.mainloop()
